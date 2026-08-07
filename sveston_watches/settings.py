5ad0e611ffd3d3185886c7236e9dbe430d306d68
ammarakhter20-collab <ammarakhter2.0@gmail.com>
2026-08-08 01:11:46 +0500

    Add Render deployment configuration (requirements.txt, build.sh, whitenoise)

--- sveston_watches/settings.py
+++ sveston_watches/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-4^ogf=(6@j^vb$h9qww1qafyr3uce!%2^pfmm=%!gffcc8m*mo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'testserver']
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
